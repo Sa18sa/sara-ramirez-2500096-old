88d32bdbb982ec03f78f24b0433387646f397396
Sa18sa <ramirezperdomoelizabeth@gmail.com>
2023-05-12 11:47:30 -0500

    cambios

--- js/media-query.js
+++ js/media-query.js
@@ -1,5 +1,24 @@
-let menuBar= document.getElementById("menuBar");
- 
+let menuBar = document.getElementById("menuBar");
+let mainNav = document.getElementById("mainNav"); 
+let mainMenu = document.getElementById("mainMenu"); 
+
 menuBar.addEventListener("click", function() {
-    alert("holis, si funciona!! :)");
+if (mainMenu.classList.contains("main-menu")) {
+    mainMenu.classList.add("main-menu-block");
+    mainMenu.classList.remove("main-menu");
+}
+else {
+    mainMenu.classList.add("main-menu");
+    mainMenu.classList.remove("main-menu-block");
+}
 });
+
+
+
+/*
+    mainMenu.classList.contains("main-menu")) {
+    mainMenu.classList.remove("main-menu");
+    mainMenu.classList.add("main-menu-block");
+    }
+    alert("holis, si funciona!! :)");*/
+
